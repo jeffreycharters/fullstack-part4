1fa34ebb19c7d68dbe740e1c4c140a5e35d278e8
dietBlue <jeff.charters@gmail.com>
2020-04-18 16:52:22 -0400

    refactor index done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
-const Blog = require('./models/blog')
+const server = http.createServer(app)
 
 
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
 })

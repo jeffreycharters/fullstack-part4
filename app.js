a1ef90cb25be6b9b940a3c0d2914c159d27eb8db
dietBlue <jeff.charters@gmail.com>
2020-04-18 16:53:22 -0400

    refactor complete i think

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 
+logger.info('connecting to', config.MONGODB_URI)
+
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
